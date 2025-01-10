1a3d54b147892d822011fbd6bdbd42fba855c0e2
SendelStudio <sendel@tutanota.com>
2025-01-10 16:44:12 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <PlasmicRootProvider Head={Head}>
-      <Component {...pageProps} />
-    </PlasmicRootProvider>
+    <>
+      {/* Partie Plasmic */}
+      <PlasmicRootProvider Head={Head}>
+        <Component {...pageProps} />
+      </PlasmicRootProvider>
+
+      {/* Partie Simple Analytics */}
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+    </>
   );
 }
